6552867545c715a8d52e7f8801c3dbc387c3b53a
ArsalanRehman <arsalanrahman008@gmail.com>
2022-12-16 11:35:26 +0300

    slight modifications in the position marker

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -7,7 +7,7 @@ exports.storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
 
 exports.fileFilter = (req, file, cb) => {
@@ -19,41 +19,43 @@ exports.fileFilter = (req, file, cb) => {
   }
 };
 
-
-
 exports.uploadImage = async (req, res) => {
-
   try {
     image = req.file;
-    const ImageURL = `${req.protocol}://${req.get('host')}/api/v1/images/getImage/${image.filename}`;
+    const ImageURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/images/getImage/${image.filename}`;
     // console.log('Image URL',ImageURL);
 
     const newPicture = new imageModel({
       path: image.path,
       type: image.fieldname,
       Date: Date.now(),
-      link: ImageURL
-    })
-    await newPicture.save().then((doc) => {
-      // console.log('Image information upload successfully');
-    }).catch((err) => {
-      console.log(err);
-    })
+      link: ImageURL,
+    });
+    await newPicture
+      .save()
+      .then((doc) => {
+        // console.log('Image information upload successfully');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     console.log(image);
     res.status(200).json({
       status: 'success',
       message: 'Image has been uploaded inside server',
       path: image.path,
-      link: ImageURL
-    })
+      link: ImageURL,
+    });
   } catch (err) {
     res.status(500).json({
       status: 'fail',
-      err
-    })
+      err,
+    });
   }
-}
+};
 exports.getImage = (req, res) => {
-  res.download('upload/' + req.params.path)
-}
+  res.download('upload/' + req.params.path);
+};
