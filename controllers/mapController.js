7f221c9f46c9c370750f4e0547751cb8588c00f2
ArsalanRehman <arsalanrahman008@gmail.com>
2023-03-07 11:12:30 +0300

    mapConvert issue (cp)

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -290,38 +290,40 @@ exports.mapConvert = async (req, res) => {
     );
     // console.log('endpoint triggered');
 
+    // const imageType = 'png';
     const imageType = req.body.imageBase64.match(
       /^data:image\/([a-z]+);base64,/
     )[1];
     const filePath = `./images/convertedImage.${imageType}`;
 
-    fs.writeFile(filePath, base64Data, 'base64', (err) => {
+     fs.writeFile(filePath, base64Data, 'base64', (err) => {
       if (err) {
         return res.status(500).send({ error: err });
       }
 
       res.status(200).send({ message: 'Image saved successfully' });
     });
-    // Read the PNG image from file
-    sharp('images/convertedImage.png')
-      .grayscale()
-      .raw()
-      .toBuffer((err, data, info) => {
-        if (err) throw err;
-
-        // Create a new PGM image with the same dimensions
-        const pgm = `P5\n${info.width} ${info.height}\n255\n`;
-
-        // Write the PGM image to file
-        const stream = fs.createWriteStream('images/my_map.pgm');
-        stream.write(pgm);
-        stream.write(data);
-        stream.end();
-      });
-    // cp -r /home/kali/catkin_ws/src/js_pkg/images/convertedImage.png /home/kali/catkin_ws/src/mir_robot/mir_gazebo/maps/
-    // spawnSync('sleep', ['5'], {
+    // spawnSync('sleep', ['3'], {
     //   shell: true,
     // });
+    // Read the PNG image from file
+    await Jimp.read('images/convertedImage.png', (err, image) => {
+      if (err) throw err;
+
+      // Convert the image to grayscale and extract the pixel data
+      const grayscale = image.grayscale().bitmap.data;
+
+      // Construct a new PGM image with the same dimensions
+      const pgm = `P5\n${image.bitmap.width} ${image.bitmap.height}\n255\n`;
+
+      // Write the PGM image to file
+      const stream = fs.createWriteStream('images/my_map.pgm');
+      stream.write(pgm);
+      stream.write(grayscale);
+      stream.end();
+    });
+    // cp -r /home/kali/catkin_ws/src/js_pkg/images/convertedImage.png /home/kali/catkin_ws/src/mir_robot/mir_gazebo/maps/
+
     spawnSync(
       'cp',
       [
